4aaea5a4cbd30b204bcb8ff05b1e576be127fab4
hotsaucejake <get@hotsaucejake.com>
2024-09-28 19:45:30 -0400

    fix angular

--- hotswapjs/public/angular/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-EONQ24HS.mjs";import"./chunk-3HCXYWQU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- hotswapjs/routes/web.php
+++ hotswapjs/routes/web.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\FrontendSwapController;
 use App\Http\Controllers\HotSwapJSController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('frontend', FrontendSwapController::class)->name('frontend');
+Route::post('frontend', FrontendSwapController::class)
+    ->name('frontend');
 
 // This route captures all routes (/{any}) and passes them to the HotSwapJSController
 // The where clause excludes any routes that start with api, allowing your API routes to function normally.
-Route::any('/{any}', HotSwapJSController::class)->where('any', '^(?!api).*$')->name('home');
+Route::any('/{any}', HotSwapJSController::class)
+    ->where('any', '^(?!api).*$')
+    ->name('home');
